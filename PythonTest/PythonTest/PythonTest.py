07e41861e0dd4eb4d79aad623c56f05da6611416
chunpengxu <chunpengxu_pansino@users.noreply.github.com>
2018-11-26 15:28:18 +0800

    Initial commit

---
+++ PythonTest/PythonTest/PythonTest.py
@@ -0,0 +1,29 @@
+import os, sys
+import time
+import wmi
+def get_disk_info():
+    tmplist=[]
+    c=wmi.WMI()
+    for physical_disk in c.Win32_DiskDrive():
+        tmplist.append(physical_disk.SerialNumber)
+        tmplist.append(physical_disk.Caption)
+        tmplist.append(str(int(physical_disk.Size)/1024/1024/1024))
+        #print(tmplist)
+        disk_str=','.join(tmplist)
+    return disk_str
+
+def get_cpu_info():
+    tmplist=[]
+    c=wmi.WMI()
+    for computer_cpu in c.Win32_Processor():
+        tmplist.append(computer_cpu.ProcessorId)
+        cpu_str=' '.join(tmplist)
+    return cpu_str
+
+if __name__=="__main__":
+    disk=get_disk_info()
+    print(disk)
+    print("-------------------------------")
+    cpu=get_cpu_info()
+    print(cpu)
+
