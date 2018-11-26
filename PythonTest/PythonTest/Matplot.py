61ef462a83b69b8b49e60e7cab9bc8bb34f9efdc
magiclsh <36143793+magiclsh@users.noreply.github.com>
2018-11-26 16:09:12 +0800

    Update Matplot.py (#1)
    update

--- PythonTest/PythonTest/Matplot.py
+++ PythonTest/PythonTest/Matplot.py
@@ -3,9 +3,13 @@ from math import radians
 import numpy as np     # installed with matplotlib
 import matplotlib.pyplot as plt
 
+def add():
+    y=1+2
+    return y
+
 def main():
     x = np.arange(0, radians(1800), radians(12))
     plt.plot(x, np.cos(x), 'b')
     plt.show()
 
-main()
+main
